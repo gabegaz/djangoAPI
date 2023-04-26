a2a0b0d283e2fb6a7c8b4b6dec02dd9ce59e2c8c
Getachew Abegaz <getabegaz@gmail.com>
2023-04-26 12:57:35 +0300

    New updates for Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-3ab@5+*y_!#79uqm-!_5sr(+_lej%(-!&2w%@h4ha6#sxq*tc!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
@@ -36,15 +36,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
 
     #local apps
     'books.apps.BooksConfig',
+    'apis.apps.ApisConfig',
+
+    #third party
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+
+STATICFILES_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
